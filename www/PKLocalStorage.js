77ae2320c0a69dcb5c243b1aec4535cb10ab77b2
Kerri Shotts <kerrishotts@gmail.com>
2014-05-07 18:38:47 -0500

    License

--- www/PKLocalStorage.js
+++ www/PKLocalStorage.js
@@ -1,5 +1,25 @@
+/*
+Kerri Shotts, (C) 2014, Version 1.0.0
 
-var pkLocalStorage = {
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be included in all copies
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var PKLocalStorage = {
   _onPauseHandlers: [],
   _onResumeHandlers: [],
   _addHandler: function _addHandler ( fn, handlerArray ) {
@@ -26,7 +46,7 @@ var pkLocalStorage = {
   _notify: function _notify ( event, handlers ) {
     handlers.forEach ( function (handler) {
       handler(event);
-    }
+    });
   },
   notifyOfPause: function() {
     this._notify ( "pause", this._onPauseHandlers );
@@ -34,6 +54,6 @@ var pkLocalStorage = {
   notifyOfResume: function() {
     this._notify ( "resume", this._onResumeHandlers );
   }
-}
+};
 
-module.exports = pkLocalStorage;
+module.exports = PKLocalStorage;
