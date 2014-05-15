43cc14d2937de276ca067b5626eb41cb833bcb1b
Kerri Shotts <kerrishotts@gmail.com>
2014-05-14 21:48:11 -0500

    Support iOS 6

--- src/ios/PKLocalStorage.m
+++ src/ios/PKLocalStorage.m
@@ -1,7 +1,7 @@
 //
 //  PKLocalStorage.m
 //  Created by Kerri Shotts on 5/6/14.
-//  Version 1.0.0
+//  Version 1.1.0
 //
 /*
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
@@ -38,6 +38,41 @@ OTHER DEALINGS IN THE SOFTWARE.
 
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(pageDidLoad:) name:CDVPageDidLoadNotification object:self.webView];
 }
+
+//ios6 base64 support from http://stackoverflow.com/questions/13245300/base64-encoding-for-nsstring
++ (NSString*)base64forData:(NSData*)theData 
+{
+    const uint8_t* input = (const uint8_t*)[theData bytes];
+    NSInteger length = [theData length];
+
+    static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+    NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+    uint8_t* output = (uint8_t*)data.mutableBytes;
+
+    NSInteger i;
+    for (i=0; i < length; i += 3) {
+        NSInteger value = 0;
+        NSInteger j;
+        for (j = i; j < (i + 3); j++) {
+            value <<= 8;
+
+            if (j < length) {
+                value |= (0xFF & input[j]);
+            }
+        }
+
+        NSInteger theIndex = (i / 3) * 4;
+        output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
+        output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
+        output[theIndex + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
+        output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
+    }
+
+    return [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+}
+
+
 -(void)pageDidLoad: (NSNotification *) notice
 {
   // check localStorage cache
@@ -55,7 +90,13 @@ OTHER DEALINGS IN THE SOFTWARE.
     localStorage = [NSString stringWithContentsOfFile:localStorageCache encoding:NSUTF8StringEncoding error:&err];
 
     NSData *localStorageData = [localStorage dataUsingEncoding:NSUTF8StringEncoding];
-    NSString *localStorageBase64 = [localStorageData base64EncodedStringWithOptions:0];
+    NSString *localStorageBase64;
+    if ( [localStorage respondsToSelector:@selector(base64EncodedStringWithOptions:)]) {
+      localStorageBase64 = [localStorageData base64EncodedStringWithOptions:0];
+    } else {
+      localStorageBase64 = [PKLocalStorage base64forData:localStorageData];
+    }
+    
     
     NSMutableString *js = [[NSMutableString alloc] init];
     [js appendFormat:@"(function() { var x = JSON.parse(atob('%@')); var c=0; for (var i in x) { if (x.hasOwnProperty(i)) { localStorage[i] = x[i]; c++; } } console.log('LocalStorage Restore Complete'); return c;})();", localStorageBase64];
